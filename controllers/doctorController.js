8a94efaadff525d5cc99d02764ef35db7ea5dc6e
madevara24 <adityadevara91@gmail.com>
2020-10-25 15:50:40 +0700

    Add vuetify

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -1,6 +1,4 @@
 const Doctor = require('../models/doctor');
-const Hospital = require('../models/hospital');
-const Schedule = require('../models/schedule');
 
 const doctor_index = (req, res) => {
     const param = req.query.search.toString()
